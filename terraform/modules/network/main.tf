b83f71e357c8e1ec15b672240b8f3181114c0419
Colux96 <simonecoluzzi96@gmail.com>
2024-07-29 15:29:54 +0200

    Modificato tipo ip load balancer in static

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -151,7 +151,7 @@ resource "azurerm_public_ip" "lb_public_ip" {
   name                = "lb-public-ip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic" 
+  allocation_method   = "Static" 
   sku                 = "Basic" 
 }
 
